934e186ed340aa4338f2478d07234f202d6e683d
moelshenawy <mohamedgk5@gmail.com>
2024-08-06 03:08:37 +0300

    ,,

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -217,6 +217,168 @@
 
 
 
+// import React, { useEffect, useState } from 'react';
+// import styles from './index.module.scss';
+// import Link from 'next/link';
+// import { FaLinkedin, FaFacebook, FaTwitter } from "react-icons/fa";
+// import { IoLogoInstagram } from "react-icons/io5";
+// import Image from 'next/image';
+// import { IoIosArrowDown } from "react-icons/io";
+// import { motion } from 'framer-motion';
+
+// const Navbar = () => {
+//   const [isScrolled, setIsScrolled] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       setIsScrolled(window.scrollY > 0);
+//     };
+
+//     window.addEventListener("scroll", handleScroll, { passive: true });
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <nav id='navbar' className={styles.navbar}>
+//       <motion.div
+//         initial={{ opacity: 0, }}
+//         whileInView={{ opacity: 1, }}
+//         exit={{ opacity: 0 }} className={`${styles.social_media_links} ${isScrolled ? styles.scrolled : ''}`}>
+//         <div className="container">
+//           <div className={styles.social_nav}>
+//             <div className={styles.icons_container}>
+//               <Link href={'#'} target='_blank' className={styles.icon}>
+//                 <FaLinkedin />
+//               </Link>
+//               <Link href={'#'} target='_blank' className={styles.icon}>
+//                 <IoLogoInstagram />
+//               </Link>
+//               <Link href={'#'} target='_blank' className={styles.icon}>
+//                 <FaFacebook />
+//               </Link>
+//               <Link href={'#'} target='_blank' className={styles.icon}>
+//                 <FaTwitter />
+//               </Link>
+//             </div>
+//             <div className={styles.join_us}>
+//               <button>
+//                 <Image src={'/assets/svgs/account_circle.svg'} width={20} height={20} />
+//                 <Link href={'#'}>
+//                   انضم الى عائلتنا
+//                 </Link>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </motion.div>
+
+//       <div className={`${styles.sec_navbar}  ${!isScrolled ? styles.scrolled : ''} `}>
+//         <div className={`${styles.nav} ${isScrolled ? `navbar fixed-top ` : ''}`}>
+//           <div className={`container `}>
+//             <div className={`${styles.wraper}  `}>
+//               <Link href="/">
+//                 <Image src={'/assets/svgs/logo.svg'} width={190} height={52} />
+//               </Link>
+//               <motion.div initial={{ opacity: 0, }}
+//                 whileInView={{ opacity: 1, }}
+//                 exit={{ opacity: 0 }} className={styles.pages_links}>
+//                 <ul>
+//                   <li>
+//                     <Link href={'/'}>
+//                       الرئيسية
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href={'#our_news'}>
+//                       أخبارنا وفعاليتنا
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href={'#trainings'}>
+//                       الدورات
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href={'/electronic-reading'}>
+//                       المتجر الإلكتروني
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link href={'#'}>
+//                       المقرأه الالكترونية
+//                     </Link>
+//                   </li>
+//                   <li className={styles.menu}>
+//                     <a>
+//                       <p>
+//                         التعليم
+//                       </p>
+//                     </a>
+//                     <IoIosArrowDown />
+//                     <ul className={styles.drop_down}>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           التعليم النسائي
+//                         </Link>
+//                       </li>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           تعليم الأطفال
+//                         </Link>
+//                       </li>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           الخطط الدراسية
+//                         </Link>
+//                       </li>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           نور البيان
+//                         </Link>
+//                       </li>
+//                     </ul>
+//                   </li>
+//                   <li className={styles.menu}>
+//                     <a>
+//                       <p>
+//                         عن المركز
+//                       </p>
+//                     </a>
+//                     <IoIosArrowDown />
+//                     <ul className={styles.drop_down}>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           تعليم الأطفال
+//                         </Link>
+//                       </li>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           الخطط الدراسية
+//                         </Link>
+//                       </li>
+//                       <li>
+//                         <Link href={'/learn'}>
+//                           نور البيان
+//                         </Link>
+//                       </li>
+//                     </ul>
+//                   </li>
+//                 </ul>
+//               </motion.div>
+
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav >
+//   );
+// }
+
+// export default Navbar;
+
 import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import Link from 'next/link';
@@ -225,13 +387,24 @@ import { IoLogoInstagram } from "react-icons/io5";
 import Image from 'next/image';
 import { IoIosArrowDown } from "react-icons/io";
 import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 0);
+      const sections = document.querySelectorAll('section');
+      sections.forEach((section) => {
+        const top = section.offsetTop - 50;
+        const bottom = top + section.offsetHeight;
+        if (window.scrollY >= top && window.scrollY < bottom) {
+          setActiveLink(section.getAttribute('id'));
+        }
+      });
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
@@ -241,6 +414,23 @@ const Navbar = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const currentPath = router.pathname;
+    const path = currentPath === '/' ? 'home' : currentPath.substring(1);
+    setActiveLink(path);
+  }, [router.pathname]);
+
+  const handleClick = (id) => {
+    if (id.startsWith('#')) {
+      const sectionId = id.substring(1);
+      document.getElementById(sectionId)
+
+      router.push(`#${sectionId}`)
+    } else {
+      router.push(id);
+    }
+  };
+
   return (
     <nav id='navbar' className={styles.navbar}>
       <motion.div
@@ -249,6 +439,7 @@ const Navbar = () => {
         exit={{ opacity: 0 }} className={`${styles.social_media_links} ${isScrolled ? styles.scrolled : ''}`}>
         <div className="container">
           <div className={styles.social_nav}>
+
             <div className={styles.icons_container}>
               <Link href={'#'} target='_blank' className={styles.icon}>
                 <FaLinkedin />
@@ -263,6 +454,7 @@ const Navbar = () => {
                 <FaTwitter />
               </Link>
             </div>
+
             <div className={styles.join_us}>
               <button>
                 <Image src={'/assets/svgs/account_circle.svg'} width={20} height={20} />
@@ -271,6 +463,7 @@ const Navbar = () => {
                 </Link>
               </button>
             </div>
+
           </div>
         </div>
       </motion.div>
@@ -286,30 +479,30 @@ const Navbar = () => {
                 whileInView={{ opacity: 1, }}
                 exit={{ opacity: 0 }} className={styles.pages_links}>
                 <ul>
-                  <li>
-                    <Link href={'/'}>
+                  <li className={activeLink === 'home' ? 'active' : ''}>
+                    <a onClick={() => handleClick('/')}>
                       الرئيسية
-                    </Link>
+                    </a>
                   </li>
-                  <li>
-                    <Link href={'#our_news'}>
+                  <li className={activeLink === 'our_news' ? 'active' : ''}>
+                    <a onClick={() => handleClick('#our_news')}>
                       أخبارنا وفعاليتنا
-                    </Link>
+                    </a>
                   </li>
-                  <li>
-                    <Link href={'#trainings'}>
+                  <li className={activeLink === 'trainings' ? 'active' : ''}>
+                    <a onClick={() => handleClick('#trainings')}>
                       الدورات
-                    </Link>
+                    </a>
                   </li>
-                  <li>
-                    <Link href={'/electronic-reading'}>
+                  <li className={activeLink === 'electronic-reading' ? 'active' : ''}>
+                    <a onClick={() => handleClick('/electronic-reading')}>
                       المتجر الإلكتروني
-                    </Link>
+                    </a>
                   </li>
                   <li>
-                    <Link href={'#'}>
+                    <a onClick={() => handleClick('#')}>
                       المقرأه الالكترونية
-                    </Link>
+                    </a>
                   </li>
                   <li className={styles.menu}>
                     <a>
@@ -319,25 +512,25 @@ const Navbar = () => {
                     </a>
                     <IoIosArrowDown />
                     <ul className={styles.drop_down}>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           التعليم النسائي
-                        </Link>
+                        </a>
                       </li>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           تعليم الأطفال
-                        </Link>
+                        </a>
                       </li>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           الخطط الدراسية
-                        </Link>
+                        </a>
                       </li>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           نور البيان
-                        </Link>
+                        </a>
                       </li>
                     </ul>
                   </li>
@@ -349,30 +542,31 @@ const Navbar = () => {
                     </a>
                     <IoIosArrowDown />
                     <ul className={styles.drop_down}>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           تعليم الأطفال
-                        </Link>
+                        </a>
                       </li>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           الخطط الدراسية
-                        </Link>
+                        </a>
                       </li>
-                      <li>
-                        <Link href={'/learn'}>
+                      <li className={activeLink === 'learn' ? 'active' : ''}>
+                        <a onClick={() => handleClick('/learn')}>
                           نور البيان
-                        </Link>
+                        </a>
                       </li>
                     </ul>
                   </li>
                 </ul>
               </motion.div>
-
             </div>
           </div>
         </div>
       </div>
+
+
     </nav >
   );
 }
