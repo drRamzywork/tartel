83b95dfd54faa688d9609a00ab013323010b256a
moelshenawy <mohamedgk5@gmail.com>
2024-08-08 13:46:15 +0300

    adding google map script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ar" dir="rtl">
-      <Head />
+      <Head>
+        <script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&v=weekly&libraries=marker`}
+          defer
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
